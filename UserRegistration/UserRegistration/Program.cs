0fca01f139c28c833aa7568bf9ea75d42dba21ba
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-29 15:17:26 +0530

    [Rajesh] Added : User need to Enter Valid First Name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿namespace UserRegistration
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to User Registration Problem");
+            while (true)
+            {
+                Console.WriteLine("\n1. Enter valid First Name");
+
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        RegisterUser registerUser = new RegisterUser();
+                        Console.WriteLine(registerUser.Name("First"));
+                        break;
+                    default:
+                        Console.WriteLine("Please enter correct choice");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/RegisterUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class RegisterUser
+    {
+        public string Name(string name)
+        {
+            //Regular expression or validating first name
+            string REGEX_FIRSTNAME = "^[A-Z][A-Za-z]{2,}$";
+
+            Console.Write("Enter " + name + " Name for validation : ");
+            string enteredName = Console.ReadLine();
+
+            //if enteredName matches with our expression then 2nd part will execute otherwise 3rd part
+            return Regex.IsMatch(enteredName, REGEX_FIRSTNAME) ? (name + " Name is valid") : (name + " Name is invalid");
+        } 
+    }
+}
