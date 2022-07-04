cbed1918845ace548d7141fd00420245a27068a6
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-07-04 17:48:17 +0530

    [Rajesh] Added : MSTest Method for User Registration

---
+++ UserRegistration/UserRegistration/RegisterForTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class RegisterForTests
+    {
+        string REGEX_NAME = "^[A-Z][A-Za-z]{2,}$";
+
+        string REGEX_EMAIL = "^([A-Za-z]){3,}(([_.+-]?)([0-9A-Za-z]{1,}))*@[a-z0-9]+[.](com|net){0,1}([.]((com)|([a-z]{2}){0,1})){0,1}$";
+
+        string REGEX_CELLNO = "^[1-9]{1,3}[ ][6-9]{1}[0-9]{9}$";
+
+        string REGEX_PASS = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^0-9a-zA-Z])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$";
+
+        public string Name(string name)
+        {
+            Regex regex = new Regex(REGEX_NAME);
+
+            bool result = regex.IsMatch(name);
+            if (result)
+            {
+                return name;
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public string Email(string email)
+        {
+            Regex regex = new Regex(REGEX_EMAIL);
+
+            bool result = regex.IsMatch(email);
+            if (result)
+            {
+                return email;
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public string Mobile(string cell)
+        {
+            Regex regex = new Regex(REGEX_CELLNO);
+
+            bool result = regex.IsMatch(cell);
+            if (result)
+            {
+                return cell;
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        public string Password(string pass)
+        {
+            Regex regex = new Regex(REGEX_PASS);
+
+            bool result = regex.IsMatch(pass);
+            if (result)
+            {
+                return pass;
+            }
+            else
+            {
+                return default;
+            }
+        }
+    }
+}
---
+++ UserRegistration/UserRegistrationTests/UnitTest1.cs
@@ -0,0 +1,69 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistration;
+
+namespace UserRegistrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [DataRow("Rajesh", "Rajesh")]
+        [DataRow("Ra", null)]
+
+        [DataRow("Ambavale", "Ambavale")]
+        [DataRow("A", null)]
+        public void Name(string a, string expected)
+        {
+            RegisterForTests register = new RegisterForTests();
+            string actual = register.Name(a);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow("abc@yahoo.com", "abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com", "abc-100@yahoo.com")]
+        [DataRow("abc-100@abc.net", "abc-100@abc.net")]
+        [DataRow("abc.100@yahoo.com", "abc.100@yahoo.com")]
+        [DataRow("abc.100@abc.com.au", "abc.100@abc.com.au")]
+        [DataRow("abc@gmail.com.com", "abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com", "abc+100@gmail.com")]
+        public void Email(string a, string expected)
+        {
+            RegisterForTests register = new RegisterForTests();
+
+            string actual = register.Email(a);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        [DataRow("9 9967872990", "9 9967872990")]
+        [DataRow("91 9967872990", "91 9967872990")]
+        [DataRow("1 1000987267", null)]
+        [DataRow("919967872990", null)]
+        [DataRow("91 996787", null)]
+        public void MobileNumber(string a, string expected)
+        {
+            RegisterForTests validate = new RegisterForTests();
+
+            string actual = validate.Mobile(a);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow("9D*jkscu", "9D*jkscu")]
+        [DataRow("S&dfhkkdkwn", null)]
+        [DataRow("123sdf34", null)]
+        public void Password(string a, string expected)
+        {
+            RegisterForTests register  = new RegisterForTests();
+
+            string actual = register.Password(a);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
